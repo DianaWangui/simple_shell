02884a4fe2c76f8ed5c3bc90690c19d0769c1c3c
DianaWangui <dianawanguikt048@gmail.com>
2023-10-09 23:51:45 +0300

    Handling user input

--- main.c
+++ main.c
@@ -6,18 +6,67 @@
  */
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH];
+	int char_read;
 
-	while (1)
+	size_t len = 0;
+
+	char *line = NULL;
+
+	char *delimiter = " ";
+
+	char *input_command;
+
+	write(1, "Simple_shell$ ", 14);
+
+	char_read = getline(&line, &len, stdin);
+	if (char_read == -1)
 	{
-		printf("simple_shell$");
-		if(fgets(input, sizeof(input), stdin) == NULL)
-		{
-			printf("\nExiting simple_shell.\n");
-			break();
-		}
-		input[strcspn(input, "\n")] = '\0';
-		exec_command(input);
+		perror("Error handling input");
+		exit(-1);
 	}
-	return (0);
+	else
+	{
+		input_command = strtok(line, delimiter);
+
+		while(input_command != NULL)
+		{
+			/*First handle the inbuilt command internally */
+
+			/* if not we initialize new process to handle the path */
+			pid_t child_pid = fork();
+
+			if (child_pid == -1)
+			{
+				perror("fork failed");
+				exit(-1);
+			}
+			else if (child_pid == 0) /* child process */
+			{
+				char *path_command(input_path); /* call the path function */
+				if (path_command == NULL)
+				{
+					perror("command not found");
+					exit(1);
+				}
+				execve(path_command, &input_command, __environ);
+				if (execve(path_command, &input_command, __environ) == -1)
+				{
+					perror("failed to execute command");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else if (child_pid > 0)
+			{
+				int status;
+
+				waitpid(child_pid, &status. 0);
+				if (WIFEXITED(status) && WEXISTATUS(status) != 0)
+				{
+					printf("Child process %d exited with status %d\n", child_pid, WEXISTATUS(status));
+				}
+			}
+			input_command = strtok(line, delimiter);
+		}
+		return (0);
+	}
 }
