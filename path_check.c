65b356f4de5755773bbf1b149978f30226e43421
DianaWangui <dianawanguikt048@gmail.com>
2023-10-10 15:32:24 +0300

    correcting path error

--- path_check.c
+++ path_check.c
@@ -4,28 +4,44 @@
  * @command: The command to be checked in the path
  * Return: NULL
 */
-char *get_path_check(const char *__path, char *const argv[])
+char *get_path_check(const char *command, char *const argv[])
 {
-   char *path = getenv("PATH");
+	char *path = getenv("PATH");
 
-    char *copy_path = strdup(path);
+	char copy_path = strdup(pat);
 
-    char *directory = strtok(copy_path, ":");
+	char *directory = strtok(copy_path, ":");
 
-    while(directory != NULL)
-    {
-        char *whole_path = malloc(strlen(directory) + strlen(__path) + 2);
-        sprintf(whole_path, "%s/%s", directory, __path);
+	if (path == NULL)
+	{
+		perror("PATH not set");
+		exit(-1);
+	}
+	if (copy_path == NULL)
+	{
+		perror("Failed to copy path");
+		exit(-1);
+	}
+	while(directory != NULL)
+	{
+		char *whole_path = malloc(strlen(directory) + strlen(command) + 2);
 
-        printf("Whole path is %s", whole_path);
-        if (access(whole_path, X_OK) == 0)
-        {
-            free(copy_path);
-            return (whole_path);
-        }
-        free(whole_path);
-        directory = strtok(NULL, ":");
-    }
-    free(copy_path);
-    return(NULL);
+		if (whole_path == NULL)
+		{
+			perror("Failed to allocate memory to whole path");
+			free(copy_path);
+			exit(-1);
+		}
+		sprintf(whole_path, "%s/%s", directory, command);
+		printf("Whole path is, %s\n", whole_path);
+		if (access(whole_path, X_OK) == 0)
+		{
+			free(copy_path);
+			return (whole_path);
+		}
+		free(whole_path);
+		directory = strtok(NULL, ":");
+	}
+	free(copy_path);
+	return (0);
 }
