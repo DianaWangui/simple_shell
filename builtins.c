227a29faec036c464c5fbc4285da967a182fcbc5
DianaWangui <dianawanguikt048@gmail.com>
2023-10-17 04:08:01 -0700

    Correcting testing errors

--- builtins.c
+++ builtins.c
@@ -5,7 +5,7 @@
  */
 void exit_builtin(void)
 {
-	write(1, "Exiting Myshell...\n\n", 18);
+	/* write(1, Exiting Myshell...\n\n, 18);*/
 	/* Make it sleep for 1.5 secs before exiting */
 	sleep(1.5);
 	exit(0);
