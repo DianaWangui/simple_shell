50247c6e211773b0bbcc3657e6643ba03f97d66e
DianaWangui <dianawanguikt048@gmail.com>
2023-10-17 03:54:14 -0700

    betty error

--- enviroment.c
+++ enviroment.c
@@ -5,24 +5,22 @@
  */
 void env_builtin(void)
 {
-        char *var;
-        char *value;
-        int i;
-        /*size_t len; */
+	char *var;
+	char *value;
+	int i;
 
-        for (i = 0; environ[i] != NULL; i++)
-        {
-                var = environ[i];
-                value = strchr(var, '=');
-                if (value != NULL)
-                {
-                        /* len = value - var; */
-                        if (write(STDOUT_FILENO, environ[i], strlen(environ[i])) == -1 || write(STDOUT_FILENO, "\n", 1) == -1)
-                        {
-                                perror("Error reading environment variable");
-                                exit(EXIT_FAILURE);
-                        }
-                }
-        }
-        exit(EXIT_SUCCESS);
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		var = environ[i];
+		value = strchr(var, '=');
+		if (value != NULL)
+		{
+			if (write(STDOUT_FILENO, environ[i], strlen(environ[i])) == -1)
+			{
+				perror("Error reading environment variable");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	exit(EXIT_SUCCESS);
 }
