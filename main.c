6a348a85fe3b324c7155de81273de78471f45be7
DianaWangui <dianawanguikt048@gmail.com>
2023-10-18 06:48:43 -0700

    builtin

--- main.c
+++ main.c
@@ -1,17 +1,42 @@
 #include "main.h"
+void signal_handler(int signal);
 /**
  * main - The main function to read user input
+ * @argc: argument count
+ * @argv: array of argument string
  * Return: 0 on success
  */
-int main(void)
+int main(char argc, char *argv[])
 {
+	FILE *file;
 	int char_read;
 	int char_write = 0;
 	char *prompt = "$ ";
 	size_t len = 0;
 	char *line = NULL;
 	int is_interactive = isatty(STDIN_FILENO);
+
 	signal(SIGINT, signal_handler);
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			file = fopen(argv[i], "r");
+			if (file == NULL)
+			{
+				perror("Error opening file");
+				continue;
+			}
+			while ((char_read = getline(&line, &len, file)) != -1)
+			{
+				if (char_read > 1)
+					tokenization(line);
+			}
+		}
+		fclose(file);
+	}
+	else
+	{
 
 	while (1)
 	{
@@ -49,6 +74,7 @@ int main(void)
 		{
 			tokenization(line);
 		}
+	}
 	}
 	free(line);
 	return (0);
